21f12271263c0c9e5bf28efbf427245039eddc51
Naumaan <naumaansheikh30@gmail.com>
2024-04-29 20:32:36 -0400

    initial commit: working sockets, join and create room, uuid

---
+++ client/src/context/RoomContext.tsx
@@ -0,0 +1,29 @@
+import React, { createContext, ReactNode, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import socketIOClient from "socket.io-client"; 
+
+const WS = 'http://localhost:8080';
+
+export const RoomContext = createContext<null | any>(null);
+
+const ws = socketIOClient(WS);
+
+interface RoomProviderProps {
+	children: ReactNode;
+}
+
+export const RoomProvider: React.FunctionComponent<RoomProviderProps> = ({ children }) => {
+	const navigate = useNavigate();
+	
+	const enterRoom = ({ roomId }: { roomId: 'string' }) => {
+		console.log({ roomId });
+		navigate(`/room/${roomId}`);
+	};
+	
+	useEffect(() => {
+		ws.on('room-created', enterRoom);
+	}, [])
+	return(
+		<RoomContext.Provider value={{ ws }}>{children}</RoomContext.Provider>
+	);
+}
