7753e78469ac4281565ef56d85421d58de6b8c90
Naumaan <naumaansheikh30@gmail.com>
2024-04-30 17:40:41 -0400

    Created joinable rooms with video auto play for all participants

--- server/src/room/index.ts
+++ server/src/room/index.ts
@@ -1,20 +1,48 @@
 import { Socket } from "socket.io";
 import { v4 as uuidV4 } from "uuid";
 
+// replicates a database, should change to database on deploy
+const rooms: Record<string, string[]> = {};
+
+interface IRoomParams {
+	roomId: string,
+	peerId: string,
+};
+
 export const roomHandler = (socket: Socket) => {
-	
-	const joinRoom = ({ roomId} : { roomId: 'string' }) => {
-		console.log('user joined the room.', roomId);
-		socket.join(roomId);
+
+	const joinRoom = ({ roomId, peerId } : IRoomParams) => {
+		if(rooms[roomId]) {
+			console.log('user joined the room.', roomId);
+			rooms[roomId].push(peerId);
+			socket.join(roomId);
+
+			socket.to(roomId).emit('user-joined', { peerId });
+
+			socket.emit('get-users', {
+				roomId,
+				participants: rooms[roomId],
+			});
+		}
+
+		socket.on("disconnect", () => {
+			console.log("user left the room", peerId);
+			leaveRoom({ roomId, peerId })
+		})
 	};
 
 	const createRoom = () => {
 		const roomId = uuidV4();
+		rooms[roomId] = [];
 		socket.emit('room-created', { roomId });
 		console.log('user created the room.');
 	};
 
-	socket.on('create-room', createRoom);
+	const leaveRoom = ({peerId, roomId}: IRoomParams) => {
+		rooms[roomId] = rooms[roomId].filter(id => id !== peerId);
+		socket.to(roomId).emit("user-disconnected", peerId);
+	};
 
+	socket.on('create-room', createRoom);
 	socket.on('join-room', joinRoom);
 };
