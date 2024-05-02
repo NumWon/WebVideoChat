76740b5d590084fc84826336584534728bb941e2
Naumaan <naumaansheikh30@gmail.com>
2024-05-02 01:28:36 -0400

    working on share screen button. need to update based on latest peerjs to get screen sharing for participants as well

--- client/src/context/RoomContext.tsx
+++ client/src/context/RoomContext.tsx
@@ -21,7 +21,9 @@ export const RoomProvider: React.FunctionComponent<RoomProviderProps> = ({ child
 	const [me, setMe] = useState<Peer>();
 	const [stream, setStream] = useState<MediaStream>();
 	const [peers, dispatch] = useReducer(peersReducer, {});
-	
+	const[screenSharingId, setScreenSharingId] = useState<string>("");
+	const [roomId, setRoomId] = useState<string>();
+
 	const enterRoom = ({ roomId }: { roomId: 'string' }) => {
 		console.log({ roomId });
 		navigate(`/room/${roomId}`);
@@ -33,6 +35,27 @@ export const RoomProvider: React.FunctionComponent<RoomProviderProps> = ({ child
 
 	const removePeer = (peerId: string) => {
 		dispatch(removePeerAction(peerId));
+	};
+
+	const switchStream = (stream: MediaStream) => {
+		setStream(stream);
+		setScreenSharingId(me?.id || "");
+
+		Object.values(peers).forEach((peer: any) => {
+			const connection = peer.getConnection(me?.id);
+			if (connection) {
+				const videoTrack: any = stream?.getTracks().find((track) => track.kind === "video");
+				connection.peerConnection.getSenders().find((sender: any) => sender.track.kind === "video").replaceTrack(videoTrack).catch((err: any) => console.error(err));
+			}
+		});
+	};
+
+	const shareScreen = () => {
+		if(screenSharingId) {
+			navigator.mediaDevices.getUserMedia({video: true, audio: true}).then(switchStream);
+		} else {
+			navigator.mediaDevices.getDisplayMedia({}).then(switchStream);
+		}
 	};
 	
 	useEffect(() => {
@@ -52,7 +75,26 @@ export const RoomProvider: React.FunctionComponent<RoomProviderProps> = ({ child
 		ws.on('room-created', enterRoom);
 		ws.on('get-users', getUsers);
 		ws.on('user-disconnected', removePeer);
+		ws.on("user-started-sharing ", (peerId) => setScreenSharingId(peerId));
+		ws.on("user-stopped-sharing", (peerId) => setScreenSharingId(""));
+
+		return () => {
+			ws.off("room-created");
+			ws.off("get-users");
+			ws.off("user-disconnnected");
+			ws.off("user-started-sharing");
+			ws.off("user-stopped-sharing");
+			ws.off("user-joined");
+		};
 	}, []);
+
+	useEffect(() => {
+		if (screenSharingId) {
+			ws.emit("start-sharing", {peerId: screenSharingId, roomId });
+		} else {
+			ws.emit("stop-sharing");
+		}
+	}, [screenSharingId, roomId])
 
 	useEffect(() => {
 		if (!me) return;
@@ -79,6 +121,6 @@ export const RoomProvider: React.FunctionComponent<RoomProviderProps> = ({ child
 	console.log({ peers });
 
 	return(
-		<RoomContext.Provider value={{ ws, me, stream, peers }}>{children}</RoomContext.Provider>
+		<RoomContext.Provider value={{ ws, me, stream, peers, shareScreen, screenSharingId, roomId, setRoomId }}>{children}</RoomContext.Provider>
 	);
 }
